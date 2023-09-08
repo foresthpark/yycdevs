b70afca3b27a6874d0a4ecbf1c10c25a6c0d8a07
Forest Park <foresthpark@gmail.com>
2023-09-07 21:46:17 -0700

    favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 import NavBar from "@/components/NavBar/NavBar";
 import Footer from "@/components/Footer/Footer";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
+      <Head>
+        <title>YYC Devs | Find the talent you need, locally.</title>
+        <meta
+          name="description"
+          content="Find the right developer for you, right here in Calgary"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
       <Footer />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import HeroBanner from "@/components/HeroBanner";
-import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>YYC Devs | Find the talent you need, locally.</title>
-        <meta
-          name="description"
-          content="Find the right developer for you, right here in Calgary"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <HeroBanner />
     </>
   );
